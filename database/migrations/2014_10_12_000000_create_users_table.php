1909a6d5aae2846e3ed4e827dea22214089a1f5b
rejohnbd <rejohn.cse@gmail.com>
2022-11-19 23:48:16 +0600

    Add Role Model, Migration & update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('role_id')->comment('Fk from table: roles');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->default('default.png');
             $table->rememberToken();
             $table->timestamps();
         });
